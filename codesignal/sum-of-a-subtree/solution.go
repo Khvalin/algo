aeeb89e51dbfd184f84a92706f27505ac6e3d26a
Constantine <khvalin@gmail.com>
2018-08-29 00:23:02 -0700

    sum-of-a-subtree go solution

--- codesignal/sum-of-a-subtree/solution.go
+++ codesignal/sum-of-a-subtree/solution.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"fmt"
+	"sort"
+)
+
 // Tree Definition for binary tree:
 type Tree struct {
 	Value interface{}
@@ -8,11 +13,13 @@ type Tree struct {
 }
 
 func mostFrequentSum(t *Tree) []int {
-	//sums := []int{}
-
 	var visit func(node *Tree) []int
 
 	visit = func(node *Tree) []int {
+		if node == nil {
+			return []int{}
+		}
+
 		result := []int{}
 		leftSums := []int{}
 		rightSums := []int{}
@@ -20,23 +27,82 @@ func mostFrequentSum(t *Tree) []int {
 		total, _ = (node.Value).(int)
 
 		if node.Left != nil {
-			leftSums := visit(node.Left)
-			total += leftSums[len(leftSums)-1]
+			leftSums = visit(node.Left)
+			total += leftSums[0]
 		}
 
 		if node.Right != nil {
-			rightSums := visit(node.Right)
-			total += rightSums[len(rightSums)-1]
+			rightSums = visit(node.Right)
+			total += rightSums[0]
 		}
 
-		result = append(leftSums, rightSums...)
+		result = append(result, total)
+		result = append(result, leftSums...)
+		result = append(result, rightSums...)
 
-		return append(result, total)
+		//fmt.Println(result)
+		return result
 	}
 
-	return visit(t)
+	sums := visit(t)
+	sort.Ints(sums)
+
+	freq := make(map[int]int)
+	maxFreq := 0
+
+	for i := 0; i < len(sums); i++ {
+		_, ok := freq[sums[i]]
+		if !ok {
+			freq[sums[i]] = 0
+		}
+
+		freq[sums[i]]++
+
+		if freq[sums[i]] > maxFreq {
+			maxFreq = freq[sums[i]]
+		}
+	}
+
+	result := []int{}
+	for i := 0; i < len(sums); i++ {
+		if freq[sums[i]] == maxFreq && (i == 0 || sums[i-1] != sums[i]) {
+			result = append(result, sums[i])
+		}
+	}
+
+	return result
 }
 
 func main() {
-	///**/
+	root := &Tree{Value: -2, Left: &Tree{Value: -3}, Right: &Tree{Value: 2}}
+	/*
+			t: {
+		    "value": 5,
+		    "left": {
+		        "value": 2,
+		        "left": null,
+		        "right": null
+		    },
+		    "right": {
+		        "value": -3,
+		        "left": {
+		            "value": 10,
+		            "left": null,
+		            "right": null
+		        },
+		        "right": {
+		            "value": 4,
+		            "left": null,
+		            "right": null
+		        }
+		    }
+		}
+	*/
+
+	root2 := &Tree{Value: 5, Left: &Tree{Value: 2}, Right: &Tree{Value: -3, Left: &Tree{Value: 10}, Right: &Tree{Value: 4}}}
+	root3 := &Tree{Value: 42}
+
+	fmt.Println(mostFrequentSum(root))
+	fmt.Println(mostFrequentSum(root2))
+	fmt.Println(mostFrequentSum(root3))
 }
