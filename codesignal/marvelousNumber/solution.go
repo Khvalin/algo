9674a82fdc659f4b342b12e8d57cf4690c842a33
Constantine <khvalin@gmail.com>
2018-09-16 09:09:01 -0700

    marvelousNumber solved

--- codesignal/marvelousNumber/solution.go
+++ codesignal/marvelousNumber/solution.go
@@ -5,7 +5,7 @@ import (
 )
 
 func marvelousNumber(n string) string {
-	const F, S = ('4'), ('7')
+	const F, S, Z = '4', '7', '0'
 
 	var solve func(dgts []byte) []byte
 	solve = func(dgts []byte) []byte {
@@ -31,39 +31,67 @@ func marvelousNumber(n string) string {
 		}
 
 		c4, c7 := 0, 0
-		f := L + 1
-		for i := 0; i < L; i++ {
-			if dgts[i] <= F {
+
+		inc := func(f int, clause func() bool) bool {
+			for i := f + 1; i < L; i++ {
+				dgts[i] = Z
+			}
+
+			for f >= 0 && ((clause != nil) && clause() || dgts[f] > S) {
+				if dgts[f] > F {
+					c4++
+					c7--
+				}
+				dgts[f] = Z
+				f--
+				if f >= 0 {
+					if dgts[f] == F {
+						c4--
+						c7++
+					}
+					dgts[f]++
+				}
+			}
+
+			of := f < 0
+			if of {
+				dgts = append([]byte{'1'}, dgts...)
+			}
+			return of
+		}
+		///
+
+		pr, f := false, L+1
+		for i, ch := range dgts {
+			if !pr && ch != F && ch < S {
+				pr = !inc(i, nil)
+			}
+
+			if ch <= F {
 				c4++
 			}
-			if dgts[i] > F && dgts[i] <= S {
+			if ch > F && ch <= S {
 				c7++
 			}
-			if f > i && dgts[i] > S {
+			if f > i && ch > S {
 				f = i
 			}
 		}
 
 		if f < L {
-			for i := f + 1; i < L; i++ {
-				dgts[i] = '0'
-			}
-			for f >= 0 && dgts[f] > S {
-				f--
-				if f >= 0 {
-					dgts[f]++
-				}
-			}
+			inc(f, nil)
 
-			if f < 0 {
-				dgts = append([]byte{'1'}, dgts...)
-			}
 			return solve(dgts)
 		}
 
 		if c7 > c4 {
-			return fallback()
+			clause := func() bool { return c7 > c4 }
+
+			if inc(L-1, clause) {
+				return fallback()
+			}
 		}
+
 		for i := L - 1; i >= 0; i-- {
 			ch := dgts[i]
 			if ch < S {
@@ -81,21 +109,33 @@ func marvelousNumber(n string) string {
 
 			}
 		}
+
+		if c4 != c7 {
+			return fallback()
+		}
 		return dgts
 	}
 
 	return string(solve([]byte(n)))
 }
 
-func main() { /*
-		fmt.Println(marvelousNumber("4587"))
-		fmt.Println(marvelousNumber("7779"))
-		fmt.Println(marvelousNumber("1007"))
+func main() {
 
-		fmt.Println(marvelousNumber("4478"), "4747")*/
-	fmt.Println(marvelousNumber("4449"), "4477")
+	fmt.Println(marvelousNumber("4587"))
+	fmt.Println(marvelousNumber("7779"))
+	fmt.Println(marvelousNumber("1007"))
+
+	fmt.Println(marvelousNumber("4478"), "4747")
+	fmt.Println("4449 => ", marvelousNumber("4449"), "4477")
 
 	fmt.Println(marvelousNumber("747777"), "774447")
 	fmt.Println(marvelousNumber("4849"), "7447")
 	fmt.Println(marvelousNumber("1000"), "4477")
+
+	fmt.Println(marvelousNumber("444777"), "444777")
+	fmt.Println(marvelousNumber("474747777777444449"), "474774444444777777")
+
+	fmt.Println(marvelousNumber("7777744445"), "444444777777")
+
+	fmt.Println(marvelousNumber("70070077"), "74444777")
 }
