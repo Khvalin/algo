85deeb2db9c812731244c219795252f567c864e2
Constantine <khvalin@gmail.com>
2018-12-09 11:33:43 -0800

    BFS ladderLength

--- leetcode/word-ladder/solution.go
+++ leetcode/word-ladder/solution.go
@@ -1,6 +1,28 @@
 package wordLadder
 
-import "fmt"
+import (
+	"fmt"
+)
+
+type Item struct {
+	used map[int]bool
+	path []int
+}
+
+func (item *Item) String() string {
+	return fmt.Sprintf("%b", item.path)
+}
+
+func copyItem(i *Item) *Item {
+	res := &Item{path: []int{}, used: map[int]bool{}}
+	if i != nil {
+		for key, value := range i.used {
+			res.used[key] = value
+		}
+		res.path = append(i.path, res.path...)
+	}
+	return res
+}
 
 func ladderLength(beginWord string, endWord string, wordList []string) int {
 	canTransform := func(str1, str2 string) bool {
@@ -34,33 +56,36 @@ func ladderLength(beginWord string, endWord string, wordList []string) int {
 
 	res := len(wordList) + 10
 
-	stack := []int{-1}
-	used := map[int]bool{}
-	last := map[int]int{}
+	q := []Item{Item{path: []int{-1}, used: map[int]bool{}}}
 
-	for len(stack) > 0 {
-		c := stack[len(stack)-1]
+	for len(q) > 0 {
+		v := q[0]
+		q = q[1:]
 
-		i := last[c]
-
-		for i < len(T[c]) && used[T[c][i]] {
-			i++
-		}
-		if i < len(T[c]) {
-			last[c] = i + 1
-			n := T[c][i]
-			stack = append(stack, n)
-			used[n] = true
-		} else {
-			if c >= 0 && wordList[c] == endWord && len(stack) < res {
-				res = len(stack)
+		tmp := ""
+		for _, n := range v.path {
+			if n > -1 {
+				tmp += wordList[n] + " "
 			}
-			used[c] = false
-			last[c] = 0
-
-			stack = stack[:len(stack)-1]
 		}
-		fmt.Println(stack)
+
+		fmt.Println(tmp)
+
+		last := v.path[len(v.path)-1]
+		if last > -1 && wordList[last] == endWord {
+			res = len(v.used)
+			break
+		}
+
+		for _, n := range T[last] {
+			if !v.used[n] {
+				newItem := *copyItem(&v)
+				newItem.path = append([]int{n}, v.path...)
+				newItem.used[n] = true
+
+				q = append([]Item{newItem}, q...)
+			}
+		}
 	}
 
 	if res > len(wordList) {
