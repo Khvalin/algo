7be5eb537854ce3c991d23f051239385a30ca6c7
const <>
2019-01-10 00:03:04 -0800

    longestPalindromeSubsequence start

---
+++ codesignal/longestPalindromeSubsequence/solution.go
@@ -0,0 +1,12 @@
+package longestPalindromeSubsequence
+
+import "fmt"
+import "strings"
+
+func longestPalindromeSubsequence(input string) string {
+	str := strings.Replace(input, " ", "", -1)
+
+	fmt.Println(str)
+
+	return ""
+}
---
+++ codesignal/longestPalindromeSubsequence/solution_test.go
@@ -0,0 +1,21 @@
+package longestPalindromeSubsequence
+
+import "testing"
+
+func Test_Sample1(t *testing.T) {
+	input := "ABCCBDDCE"
+	expected := "BCCB"
+	res := longestPalindromeSubsequence(input)
+	if res != expected {
+		t.Errorf("Test failed for input %s , expected %s, got %s ", input, expected, res)
+	}
+}
+
+func Test_Sample10(t *testing.T) {
+	input := "ONLY ELEVEN CHARACTERS REMAIN"
+	expected := "NEECARACEEN"
+	res := longestPalindromeSubsequence(input)
+	if res != expected {
+		t.Errorf("Test failed for input %s , expected %s, got %s ", input, expected, res)
+	}
+}
