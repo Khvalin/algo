a58ebafb78e8d879cb32c8d80bd56cd0024d9029
const <>
2019-01-10 22:14:15 -0800

    longestPalindromeSubsequence solved

--- codesignal/longestPalindromeSubsequence/solution.go
+++ codesignal/longestPalindromeSubsequence/solution.go
@@ -1,12 +1,49 @@
 package longestPalindromeSubsequence
 
-import "fmt"
-import "strings"
+import (
+	"strings"
+)
 
 func longestPalindromeSubsequence(input string) string {
 	str := strings.Replace(input, " ", "", -1)
 
-	fmt.Println(str)
+	memo := map[int]string{}
 
-	return ""
+	var findLongestPalindrome func(left, right int) string
+	findLongestPalindrome = func(left, right int) string {
+		if left > right {
+			return ""
+		}
+		key := left*(len(str)+1) + right
+		r, found := memo[key]
+		if found {
+			return r
+		}
+
+		ch := string(str[left])
+
+		for i := right; i >= left; i-- {
+			if str[i] == str[left] {
+				next := ""
+				if i == left {
+					next = ch
+				} else {
+					next = ch + findLongestPalindrome(left+1, i-1) + ch
+				}
+				if len(next) >= len(r) {
+					r = next
+				}
+			}
+		}
+
+		next := findLongestPalindrome(left+1, right)
+		if len(next) > len(r) {
+			r = next
+		}
+
+		memo[key] = r
+		return r
+	}
+
+	return findLongestPalindrome(0, len(str)-1)
 }
--- codesignal/longestPalindromeSubsequence/solution_test.go
+++ codesignal/longestPalindromeSubsequence/solution_test.go
@@ -2,6 +2,15 @@ package longestPalindromeSubsequence
 
 import "testing"
 
+func Test_Sample0(t *testing.T) {
+	input := "ABCDAD"
+	expected := "ABA"
+	res := longestPalindromeSubsequence(input)
+	if res != expected {
+		t.Errorf("Test failed for input %s , expected %s, got %s ", input, expected, res)
+	}
+}
+
 func Test_Sample1(t *testing.T) {
 	input := "ABCCBDDCE"
 	expected := "BCCB"
