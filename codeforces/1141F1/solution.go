a8794473cbf766a83c163809aac025b90d030175
Konstantin Khvalin <Konstantin_Khvalin@epam.com>
2019-04-04 20:08:40 -0700

    1141F1 solved

--- codeforces/1141F1/solution.go
+++ codeforces/1141F1/solution.go
@@ -6,6 +6,7 @@ import (
 	"os"
 )
 
+//Range ...
 type Range struct {
 	start int
 	end   int
@@ -14,71 +15,87 @@ type Range struct {
 func solve(a []int) []Range {
 	L := len(a)
 
-	sums := make([]map[int]Range, L)
-	all := map[int]int{}
+	sums := make([]map[int]int, L)
+	//	all := map[int]bool{}
 
 	for i := L - 1; i >= 0; i-- {
-		cur := a[i]
+		cur := 0
 
 		for j := i; j < L; j++ {
-			if i != j {
-				cur += a[j]
-			}
+			cur += a[j]
+
+			//			all[cur] = true
 
 			_, found := sums[i][cur]
 			if !found {
 				if sums[i] == nil {
-					sums[i] = map[int]Range{}
+					sums[i] = map[int]int{}
 				}
 
-				all[cur]++
-
-				sums[i][cur] = Range{start: i, end: j}
+				sums[i][cur] = j
 			}
 		}
 	}
 
+	memo := map[[2]int][]int{}
+
+	var calc func(start, sum int) []int
+	calc = func(start, sum int) []int {
+		hash := [2]int{start, sum}
+		out, found := memo[hash]
+
+		if found {
+			return out
+		}
+
+		out = []int{}
+
+		if start < L {
+			if next, found := sums[start][sum]; found {
+				max := []int{start}
+				for i := 1 + next; i < L; i++ {
+
+					if _, found = sums[i][sum]; found {
+						p := calc(i, sum)
+
+						if len(p) >= len(max) {
+							max = append([]int{start}, p...)
+						}
+					}
+				}
+
+				out = make([]int, len(max))
+				copy(out, max)
+			}
+		}
+
+		memo[hash] = out
+
+		return out
+	}
+
+	target := 0
 	max := -1
-	maxPath := []Range{}
+	var starts []int
+	for i := L - 1; i >= 0; i-- {
+		for s := range sums[i] {
+			c := calc(i, s)
+			if len(c) > max {
+				target = s
+				max = len(c)
 
-	for len(all) > 0 {
-		for cur := range all {
-			i := 0
-			count := 0
-			path := []Range{}
-
-			for i < L {
-				sum, found := sums[i][cur]
-
-				if !found {
-					i++
-					continue
-				}
-				delete(sums[i], cur)
-				all[cur]--
-				if all[cur] <= 0 {
-					delete(all, cur)
-				}
-
-				count++
-				path = append(path, sum)
-
-				i = sum.end + 1
-
-				if count > max {
-					max = count
-					maxPath = make([]Range, len(path))
-					copy(maxPath, path)
-				}
-			}
-
-			if cur == -5 {
-				fmt.Println(path)
+				starts = c
 			}
 		}
 	}
 
-	return maxPath
+	res := make([]Range, max)
+
+	for i, n := range starts {
+		res[i] = Range{n, sums[n][target]}
+	}
+
+	return res
 }
 
 func toInt(buf []byte) int {
