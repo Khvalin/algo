212f926b894287bc490b1196f1b1f927a23c648a
Человек <khvalin@gmail.com>
2019-04-28 01:02:08 +0000

    solution.go created online with Bitbucket

--- codeforces/1154G/solution.go
+++ codeforces/1154G/solution.go
@@ -6,22 +6,68 @@ import (
 	"os"
 )
 
-func toInt(buf []byte) int {
-	n, mul := int(0), int(1)
-	for i, v := range buf {
-		if i == 0 && v == '-' {
-			mul = int(-1)
-			continue
-		}
-		n = n*10 + int(v-'0')
+func solve(a []uint32) (int, int) {
+	const N = uint32(1e7 + 10)
+
+	min := uint64(1<<64 - 1)
+
+	m := map[uint32]int{}
+	for i, n := range a {
+		m[n] = 1 + i
 	}
 
-	return mul * n
+	var (
+		ans1 int
+		ans2 int
+	)
+
+	for gcd := uint32(1); gcd < N; gcd++ {
+		index1, index2 := -1, -1
+
+		var (
+			first  uint32
+			second uint32
+		)
+
+		for j := gcd; j < N; j += gcd {
+			i := m[j]
+			if i > 0 {
+				if index1 > 0 {
+					second = j
+					index2 = i
+
+					break
+				} else {
+					first = j
+					index1 = i
+				}
+			}
+		}
+
+		if first > 0 && second > 0 {
+			gres := uint64(first*second) / uint64(gcd)
+
+			if gres < min {
+				min = gres
+				ans1, ans2 = index1, index2
+			}
+		}
+	}
+
+	if ans1 > ans2 {
+		ans1, ans2 = ans2, ans1
+	}
+
+	return ans1, ans2
 }
 
-func solve(a []int) (int, int) {
-	fmt.Println(a)
-	return 0, 0
+func toInt(buf []byte) uint32 {
+	n := uint32(0)
+	for _, v := range buf {
+		n = n*10 + uint32(v-'0')
+	}
+
+	return n
 }
 
 func main() {
@@ -29,7 +75,7 @@ func main() {
 
 	fmt.Scan(&n)
 
-	a := make([]int, n)
+	a := make([]uint32, n)
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
 	for i := range a {
