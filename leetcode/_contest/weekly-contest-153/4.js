6dd917383b31746adab066fdadad86c49125597f
Constantine <khvalin@gmail.com>
2019-09-15 01:01:19 -0700

    1187. Make Array Strictly Increasing solved

--- leetcode/_contest/weekly-contest-153/4.js
+++ leetcode/_contest/weekly-contest-153/4.js
@@ -1,44 +1,37 @@
-const memo = {}
-
 /**
  * @param {number[]} arr1
  * @param {number[]} arr2
  * @return {number}
  */
 var makeArrayIncreasing = function(arr1, arr2) {
-  const calc = (arr1, arr2, prev = Number.NEGATIVE_INFINITY) => {
-    const key = prev.toString() + ' -- ' + arr1.length + ' -- ' + arr2.length
+  const memo = {}
 
-    if (memo[key]) {
-      return memo[key]
-    }
+  const calc = (k, l, prev = Number.NEGATIVE_INFINITY) => {
+    let r1 = Number.POSITIVE_INFINITY
+    let r2 = Number.POSITIVE_INFINITY
 
-    if (arr1.length === 0) {
-      memo[key] = 0
+    if (k >= arr1.length) {
       return 0
     }
 
-    while (arr2.length && arr2[0] <= prev) {
-      arr2.shift()
+    const key = `${k}_${l}_${prev}`
+    if (key in memo) {
+      return memo[key]
     }
 
-    if (prev >= arr1[0] && !arr2.length) {
-      memo[key] = Number.POSITIVE_INFINITY
-      return Number.POSITIVE_INFINITY
+    while (l < arr2.length && arr2[l] <= prev) {
+      l++
     }
 
-    let res1 = Number.POSITIVE_INFINITY
-    if (arr1[0] > prev) {
-      res1 = calc([ ...arr1.slice(1) ], [ ...arr2 ], arr1[0])
+    if (l < arr2.length) {
+      r1 = 1 + calc(k + 1, l + 1, arr2[l])
     }
 
-    let res2 = Number.POSITIVE_INFINITY
-    if (arr2.length) {
-      //  console.log(arr2)
-      res2 = 1 + calc([ ...arr1.slice(1) ], [ ...arr2.slice(1) ], arr2[0])
+    if (arr1[k] > prev) {
+      r2 = calc(k + 1, l, arr1[k])
     }
 
-    const r = Math.min(res1, res2)
+    const r = Math.min(r1, r2)
     memo[key] = r
 
     return r
@@ -46,25 +39,30 @@ var makeArrayIncreasing = function(arr1, arr2) {
 
   arr2 = arr2.sort((a, b) => a - b)
 
-  const res = calc(arr1, arr2)
+  const res = calc(0, 0)
 
   return res > arr1.length ? -1 : res
 }
 
 let res
 /* 
-res = makeArrayIncreasing([ 1, 2, 3, 4, 4 ], [ 0, 1, 2, 3 ])
+res = makeArrayIncreasing([ 1, 5, 3, 6, 7 ], [ 1, 3, 2, 4 ])
 console.log(res)
 
-res = makeArrayIncreasing([ 1, 5, 3, 6, 7 ], [ 1, 3, 2, 4 ])
+
+
+res = makeArrayIncreasing([ 1, 5, 3, 6, 7 ], [ 1, 6, 3, 3 ])
+console.log(res)
+ */
+
+res = makeArrayIncreasing([ 1, 5, 3, 6, 7 ], [ 1, 6, 3, 3 ])
+console.log(res)
+
+res = makeArrayIncreasing([ 1, 2, 3, 4, 4 ], [ 0, 1, 2, 3 ])
 console.log(res)
 
 res = makeArrayIncreasing([ 1, 5, 3, 6, 7 ], [ 4, 3, 1 ])
 console.log(res)
 
-res = makeArrayIncreasing([ 1, 5, 3, 6, 7 ], [ 1, 6, 3, 3 ])
-console.log(res)
- */
 res = makeArrayIncreasing([ 0, 11, 6, 1, 4, 3 ], [ 5, 4, 11, 10, 1, 0 ])
-
 console.log(res)
