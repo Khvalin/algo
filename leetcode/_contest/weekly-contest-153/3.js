62e06aa9a794a62db9e04e68bd7bf58f47d6deff
Constantine <khvalin@gmail.com>
2019-09-15 00:35:50 -0700

    1186. Maximum Subarray Sum with One Deletion solution

--- leetcode/_contest/weekly-contest-153/3.js
+++ leetcode/_contest/weekly-contest-153/3.js
@@ -1,46 +1,30 @@
 /**
+ * 
+ *Given an array of integers, return the maximum sum for a non-empty subarray (contiguous elements) with at most one element deletion. In other words, you want to choose a subarray and optionally delete one element from it so that there is still at least one element left and the sum of the remaining elements is maximum possible.
+
+Note that the subarray needs to be non-empty after deleting one element.
+ */
+
+//Just maintain two variables, sum is the current maximal sum without any delete and delete is the current maximal sum with just one delete.
+
+/**
  * @param {number[]} arr
  * @return {number}
  */
 var maximumSum = function(arr) {
-  let res = 0
-  let d = null
+  let res = arr[0]
+  let [ sum, d ] = [ 0, 0 ]
 
-  let r = Math.max(...arr)
-  if (r < 0) {
-    return r
-  }
-
-  let s = 0
-  let sub = 0
   for (const n of arr) {
-    if (n >= 0) {
-      s += n
-      sub += n
+    if (n < 0) {
+      res = Math.max(res, n)
+      d = Math.max(sum, d + n)
+      sum = Math.max(sum + n, 0)
     } else {
-      if (n < 0 && d === null) {
-        if (s > 0) {
-          d = n
-        }
-        sub = 0
-      } else {
-        if (d > n) {
-          s += d
-          d = n
-        } else {
-          s += n
-        }
-
-        if (sub > s) {
-          s = sub
-          d = n
-        }
-        sub = 0
-      }
+      sum += n
+      d += n
+      res = Math.max(res, d, sum)
     }
-
-    res = Math.max(res, s)
-    console.log(n, s, d)
   }
 
   return res
@@ -48,10 +32,9 @@ var maximumSum = function(arr) {
 
 let res = maximumSum([ -5, -6, -23, 14, 1, -8, -1, 8, 1, -16, 15, 22, 21, 14, 5, -4, -3, 14, -1, 6, -17, 22, -19 ])
 console.log(res)
-/* 
+
 res = maximumSum([ 10, -2, -2, 30 ])
 console.log(res)
 
 res = maximumSum([ -1, -1, -1, -1 ])
 console.log(res)
- */
