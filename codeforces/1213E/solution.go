6a9b96b0106c415dce0695a8aebbd3d20f476380
Constantine <khvalin@gmail.com>
2019-09-17 23:48:26 -0700

    take 2

--- codeforces/1213E/solution.go
+++ codeforces/1213E/solution.go
@@ -6,15 +6,19 @@ import (
 
 const empty = '!'
 
-func solve(n uint32, s1, s2 []rune) string {
+func solve(n uint32, s1, s2 []rune) (bool, string) {
 	const ABC = "abc"
 
-	l := int64(3 * n)
+	m := uint32(n)
+	if n < m {
+		m = n
+	}
+	l := int64(3 * m)
 
 	count := map[rune]uint32{
-		'a': n,
-		'b': n,
-		'c': n,
+		'a': m,
+		'b': m,
+		'c': m,
 	}
 
 	r := make([]rune, l)
@@ -27,54 +31,103 @@ func solve(n uint32, s1, s2 []rune) string {
 	bl[s1[0]][s1[1]] = true
 	bl[s2[0]][s2[1]] = true
 
-	getOptions := func(min, prev rune) []rune {
+	getOptions := func(min, prev rune) rune {
 		//TODO: add order
-		r := make([]rune, 0, 3)
 
 		for _, ch := range ABC {
 			if ch >= min && count[ch] > 0 && !bl[prev][ch] {
-				r = append(r, ch)
+				return ch
 			}
 		}
 
-		return r
+		return empty
 	}
 
-	for i := int64(0); i >= 0 && i < l; {
+	i := int64(0)
+	for i >= 0 && i < l {
 		prev, s := empty, empty
 
 		if i > 0 {
 			prev = r[i-1]
 		}
 
-		if r[i] >= 'a' {
+		if r[i] > empty {
 			s = r[i] + 1
 		}
 
 		found := false
-		options := getOptions(s, prev)
+		next := getOptions(s, prev)
 
-		t := r[i]
-		for _, ch := range options {
-			t = ch
-			found = true
-			break
-		}
+		found = next != empty //&& (i != l-1 || !bl[next][r[0]])
 
 		if found {
-			count[t]--
-			r[i] = t
+			r[i] = next
+			count[next]--
 			i++
 		} else {
-			r[i] = empty
-			count[t]++
 			i--
+			if i >= 0 && r[i] > empty {
+				count[r[i]]++
+				if i < l-1 {
+					r[i+1] = empty
+				}
+			}
 		}
 
-		fmt.Println(string(r), count)
+		//	fmt.Println(string(r), count)
 	}
 
-	return string(r)
+	if i < 0 {
+		return false, ""
+	}
+
+	res := string(r)
+	/* if n > m {
+		res = strings.Repeat(res, int(n>>1))
+
+		if n%2 > 0 {
+			r = []rune{empty, empty, empty}
+
+			count['a'] = 1
+			count['b'] = 1
+			count['c'] = 1
+
+			for i := 0; i < 3 && i >= 0; {
+				prev, s := rune(res[len(res)-1]), empty
+
+				if i > 0 {
+					prev = r[i-1]
+				}
+
+				if r[i] > empty {
+					s = r[i] + 1
+				}
+
+				found := false
+				next := getOptions(s, prev)
+
+				found = next != empty
+
+				if found {
+					count[next]--
+					r[i] = next
+					i++
+				} else {
+					i--
+					if r[i] > empty {
+						count[r[i]]++
+						if i < len(r)-1 {
+							r[i+1] = empty
+						}
+					}
+				}
+			}
+
+			res += string(r)
+		}
+	} */
+
+	return true, res
 }
 
 func main() {
@@ -84,7 +137,12 @@ func main() {
 	fmt.Scan(&n)
 	fmt.Scan(&s1, &s2)
 
-	res := solve(n, ([]rune)(s1), ([]rune)(s2))
+	f, res := solve(n, ([]rune)(s1), ([]rune)(s2))
 
-	fmt.Println(res)
+	if f {
+		fmt.Println("YES")
+		fmt.Println(res)
+	} else {
+		fmt.Println("NO")
+	}
 }
