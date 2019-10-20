5cda27b80666ebb14337bc1673aab5ac12dd408f
Constantine <khvalin@gmail.com>
2019-10-20 10:15:46 -0700

    3 sort of solved

--- leetcode/_contest/weekly-contest-159/3.js
+++ leetcode/_contest/weekly-contest-159/3.js
@@ -6,66 +6,36 @@ var balancedString = function(s) {
   const counts = {}
   const l = s.length / 4
 
-  const overflow = {}
-  const d = {}
-
   for (let i = 0; i < s.length; i++) {
     const ch = s[i]
 
     counts[ch] = counts[ch] || 0
-    d[ch] = d[ch] || [ -1 ]
     counts[ch]++
-
-    d[ch][counts[ch]] = i
-
-    if (counts[ch] > l) {
-      overflow[ch] = counts[ch] - l
-    }
-  }
-
-  for (const ch of 'QWER') {
-    counts[ch] = 0
-  }
-
-  if (Object.keys(overflow).length === 0) {
-    return 0
   }
 
   let r = Number.POSITIVE_INFINITY
+  let j = 0
 
-  outer: for (let i = 0; i < s.length; i++) {
+  for (let i = 0; i < s.length; i++) {
     const ch = s[i]
 
-    counts[ch]++
+    counts[ch]--
 
-    if (!(ch in overflow)) {
-      continue
+    while (j < s.length && Object.values(counts).every((c) => c <= l)) {
+      r = Math.min(r, i - j + 1)
+      counts[s[j++]]++
     }
-
-    let len = 1
-    for (const c in overflow) {
-      if (counts[c] < overflow[c]) {
-        continue outer
-      }
-
-      //  console.log(i, counts[c], overflow[c], d[c][counts[c] - overflow[c]])
-
-      if (c !== ch || overflow[c] > 1) {
-        len = Math.max(len, i - d[c][counts[c] - overflow[c]])
-      }
-    }
-
-    r = Math.min(len, r)
   }
 
   return r
 }
 
-console.log(3, balancedString('WQWRQQQW'))
+console.log(balancedString('QQQQ'))
+
 console.log(4, balancedString('WWQQRRRRQRQQ'))
+
+console.log(3, balancedString('WQWRQQQW'))
 
 console.log(balancedString('QEQEWWEQ'))
 console.log(balancedString('QWER'))
 console.log(balancedString('QQWE'))
-
-console.log(balancedString('QQQQ'))
