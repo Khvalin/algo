41a81a462c745b124f54a1c754c3ddef9845c66d
Konstantin Khvalin <konstantin_khvalin@epam.com>
2019-11-13 18:10:20 -0800

    1256D-input

--- codeforces/1256D/Program.cs
+++ codeforces/1256D/Program.cs
@@ -1,12 +1,68 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _1256D
 {
   class Program
   {
+
+    static bool[] Solve(bool[] input, long k)
+    {
+      var L = input.Length;
+
+      var left = L;
+      var zeroes = new List<int>();
+
+      for (var i = 0; i < L; i++)
+      {
+        if (input[i])
+        {
+          if (left >= L)
+          {
+            left = i;
+          }
+        }
+        else
+        {
+          zeroes.Add(i);
+        }
+
+
+      }
+
+      if (left >= L)
+      {
+        return input;
+      }
+
+
+
+      return input;
+    }
+
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      var q = int.Parse(Console.ReadLine());
+
+      for (var i = 0; i < q; i++)
+      {
+        var line = Console.ReadLine().Split(" ");
+        var n = int.Parse(line[0]);
+        var k = long.Parse(line[1]);
+
+        var input = new bool[n];
+        var s = Console.ReadLine();
+
+        for (var j = 0; j < n; j++)
+        {
+          input[j] = s[j] == '1';
+        }
+
+        input = Solve(input, k);
+
+        Console.WriteLine(String.Join("", input.Select(b => b ? 1 : 0)));
+      }
     }
   }
 }
