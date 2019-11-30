438ccb60a9164905c4fd346a0465394ebc8f5286
Constantine <khvalin@gmail.com>
2019-11-30 08:23:31 -0800

    semi

--- leetcode/_contest/bi-weekly-contest-14/1.js
+++ leetcode/_contest/bi-weekly-contest-14/1.js
@@ -3,28 +3,28 @@
  * @return {string}
  */
 var toHexspeak = function(num) {
-  const s = parseInt(num).toString(16).toUpperCase()
-  let res = ''
+  const s = parseInt(num).toString(16).toUpperCase();
+  let res = '';
 
   for (const ch of s) {
     if (ch === '1') {
-      res += 'I'
-      continue
+      res += 'I';
+      continue;
     }
     if (ch === '0') {
-      res += 'O'
-      continue
+      res += 'O';
+      continue;
     }
 
     if ('ABCDEFIO'.indexOf(ch) < 0) {
-      res = 'ERROR'
-      break
+      res = 'ERROR';
+      break;
     }
-    res += ch
+    res += ch;
   }
-  return res
-}
+  return res;
+};
 
 //console.log(toHexspeak(257))
 //console.log(toHexspeak(3))
-console.log(toHexspeak('747823223228'))
+console.log(toHexspeak('747823223228'));
