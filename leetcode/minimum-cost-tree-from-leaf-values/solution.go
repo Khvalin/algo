ac73a210d5cdd6d815382cdbd26e6592237b6a33
Constantine <khvalin@gmail.com>
2020-02-04 00:36:49 -0800

    minimum-cost-tree-from-leaf-values take 2

--- leetcode/minimum-cost-tree-from-leaf-values/solution.go
+++ leetcode/minimum-cost-tree-from-leaf-values/solution.go
@@ -4,109 +4,115 @@ import "fmt"
 
 // The value of each non-leaf node is equal to the product of the largest leaf value in its left and right subtree respectively.
 func mctFromLeafValues(arr []int) int {
-  const inf = 4001*19
+	const inf = 4001 * 19
 
-  type state struct {
-    left, right int
-  }
-  
-  type tree struct {
-    max, sum int
-  }
-  
-  combineTrees := func(left, right tree)tree {
-    if left.max < 0 {
-      return right
-    }
-    
-    if right.max < 0 {
-      return left
-    }
-    max := left.max
-    sum := right.max * left.max + left.sum  + right.sum 
-    if sum > inf {
-      sum = inf
-    }
-    
-    if right.max > max {
-      max = right.max
-    }
+	type state struct {
+		left, right int
+	}
 
-    return tree{max: max, sum: sum }
-  }
+	type tree struct {
+		max, sum int
+	}
 
-/*
-  pows := make([]bool, len(arr) + 2)
-  for i := 2; i < len(pows); i *= 2 {
-    pows[i] = true
-  }
-  */
+	zeroTree := tree{max: -1, sum: -1}
 
-  memo := map[state]tree{}
-  for i := 1; i < len(arr);i++ {
-    val := arr[i - 1] * arr[i]
-    
-    t := tree{max: arr[i], sum: val}
-    if t.max < arr[i - 1] {
-      t.max = arr[i - 1]
-    }
-    memo[state{i - 1, i}] = t
-    
-    memo[state{i, i}] = tree{max: arr[i]}
-  }
-  memo[state{0, 0}] = tree{max: arr[0]}
-  
-  infTree := tree{max: inf, sum: inf}
+	combineTrees := func(left, right tree) tree {
+		if left == zeroTree {
+			return right
+		}
 
-  var solve func(left, right int) tree
-  solve = func(left, right int) tree {
-    if left < 0 || right >= len(arr) {
-      return tree{max: -1, sum: -1}
-    }
-    if left > right {
-      left = right
-    }
-    if right < left {
-      right = left
-    }
-    
-    st := state{left, right}
+		if right == zeroTree {
+			return left
+		}
 
-    if res, found := memo[st]; found {
-      return res
-    }
+		max := left.max
+		sum := right.max*left.max + left.sum + right.sum
+		if sum > inf {
+			sum = inf
+		}
 
-    res := infTree
-    for i := left; i < right; i++ {
-      mid := solve(i, i + 1)
-      variants := []tree{
-        combineTrees(combineTrees(solve(left,  i- 2), combineTrees(solve(left - 1, left - 1), mid)), solve(i + 2, right)),
-        combineTrees(combineTrees(solve(left, i - 1), mid), solve(i + 2, right)),
-        
-        combineTrees(solve(left, i - 1), combineTrees(mid, solve(i + 2, right)) ),
-        combineTrees(solve(left, i - 1), combineTrees(combineTrees(mid, solve(i+2, i + 2)), solve(i + 3, right)) ),
-      }
+		if right.max > max {
+			max = right.max
+		}
 
-      for _, v := range variants {
-        if v.sum < res.sum {
-          res = v
-        }
-      }
-    }
-    
-   memo[st] = res
+		return tree{max: max, sum: sum}
+	}
 
-    return res
-  }
+	memo := map[state]tree{}
+	for i := 1; i < len(arr); i++ {
+		val := arr[i-1] * arr[i]
 
-  tr := solve(0, len(arr) - 1)
+		t := tree{max: arr[i], sum: val}
+		if t.max < arr[i-1] {
+			t.max = arr[i-1]
+		}
+		memo[state{i - 1, i}] = t
 
-  return tr.sum
+		memo[state{i, i}] = tree{max: arr[i]}
+	}
+	memo[state{0, 0}] = tree{max: arr[0]}
+
+	infTree := tree{max: inf, sum: inf}
+
+	var solve func(left, right int) tree
+	solve = func(left, right int) tree {
+		if left < 0 || right >= len(arr) {
+			return zeroTree
+		}
+
+		st := state{left, right}
+
+		if res, found := memo[st]; found {
+			return res
+		}
+
+		res := infTree
+		for i := left; i < right; i++ {
+			mid := solve(i, i+1)
+
+			leftSibling, rightSibling := zeroTree, zeroTree
+			leftSubtree, rightSubtree := zeroTree, zeroTree
+
+			if i > left {
+				leftSibling = solve(i-1, i-1)
+				if i > left+1 {
+					leftSubtree = solve(left, i-2)
+				}
+			}
+
+			if i < right-1 {
+				rightSibling = solve(i+2, i+2)
+				if i < right-2 {
+					rightSubtree = solve(i+2, right)
+				}
+			}
+
+			variants := []tree{
+				combineTrees(combineTrees(combineTrees(leftSubtree, leftSibling), mid), combineTrees(rightSibling, rightSubtree)),
+				combineTrees(combineTrees(leftSubtree, combineTrees(leftSibling, mid)), combineTrees(rightSibling, rightSubtree)),
+				combineTrees(combineTrees(leftSubtree, leftSibling), combineTrees(combineTrees(mid, rightSibling), rightSubtree)),
+				combineTrees(combineTrees(leftSubtree, leftSibling), combineTrees(mid, combineTrees(rightSibling, rightSubtree))),
+			}
+
+			for _, v := range variants {
+				if v.sum < res.sum {
+					res = v
+				}
+			}
+		}
+
+		memo[st] = res
+
+		return res
+	}
+
+	tr := solve(0, len(arr)-1)
+
+	return tr.sum
 }
 
-
 func main() {
-  fmt.Println(32, mctFromLeafValues([]int{6,2,4}))
-//  fmt.Println(15, mctFromLeafValues([]int{1, 3, 3, 1}))
-//  fmt.Println(541, mctFromLeafValues([]int{15,10, 2, 3, 4, 1, 1, 1, 5, 4, 3, 6, 15}))
+	fmt.Println(32, mctFromLeafValues([]int{6, 2, 4}))
+	fmt.Println(15, mctFromLeafValues([]int{1, 3, 3, 1}))
+	fmt.Println(541, mctFromLeafValues([]int{15, 10, 2, 3, 4, 1, 1, 1, 5, 4, 3, 6, 15}))
 }
