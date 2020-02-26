b438e15e79dff865248ad412828e916f8098e5c5
Konstantin Khvalin <Khvalin@users.noreply.github.com>
2020-02-25 20:13:26 -0800

    1307D take2

--- codeforces/1307D/solution.go
+++ codeforces/1307D/solution.go
@@ -3,20 +3,20 @@ package main
 import (
 	"bufio"
 	"fmt"
-  "sort"
 	"os"
+	//	"sort"
 )
 
-func toInt(buf []byte) uint32 {
-	n := uint32(0)
+func toInt(buf []byte) int {
+	n := int(0)
 	for _, v := range buf {
-		n = n*10 + uint32(v-'0')
+		n = n*10 + int(v-'0')
 	}
 
 	return n
 }
 
-func readData() (n, m, k uint32, connected [][]bool, special []uint32) {
+func readData() (n, m, k int, connected [][]int, special []int) {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
 
@@ -29,37 +29,81 @@ func readData() (n, m, k uint32, connected [][]bool, special []uint32) {
 	scanner.Scan()
 	k = toInt(scanner.Bytes())
 
-	special = make([]uint32, k)
+	special = make([]int, k)
 	for i := range special {
 		scanner.Scan()
 		special[i] = toInt(scanner.Bytes())
 	}
-  _ = sort.Ints
+	// sort.Slice(special, func(i, j int) bool {
+	// 	return special[i] < special[j]
+	// })
 
-	connected = make([][]bool, 1+n)
-	for i := uint32(0); i < m; i++ {
+	connected = make([][]int, 1+n)
+	for i := int(0); i < m; i++ {
 		scanner.Scan()
 		x := toInt(scanner.Bytes())
 
 		scanner.Scan()
 		y := toInt(scanner.Bytes())
 
-		if connected[x] == nil {
-			connected[x] = make([]bool, 1+n)
-		}
-
-		if connected[y] == nil {
-			connected[y] = make([]bool, 1+n)
-		}
-
-		connected[x][y], connected[y][x] = true, true
+		connected[x] = append(connected[x], y)
+		connected[y] = append(connected[y], x)
 	}
 
 	return
 }
 
-func solve (n, m, k uint32, connected [][]bool, special []uint32) uint32 {
-  return 0
+func solve(n, m, k int, connected [][]int, special []int) int {
+	d, path := make([]int, n+1), make([]int, n+1)
+	for i := range d {
+		d[i] = m + 2
+	}
+
+	isSpecial := make([]bool, n+2)
+	for _, n := range special {
+		isSpecial[n] = true
+	}
+
+	q := []int{1}
+	d[1] = 0
+
+	for len(q) > 0 {
+		p := q[len(q)-1]
+		q = q[:len(q)-1]
+
+		for _, n := range connected[p] {
+			if d[n] > m {
+				d[n] = d[p] + 1
+				q = append(q, n)
+
+				path[n] = p
+			}
+		}
+	}
+
+	res := 0
+
+	c := 0
+	last := -1
+	for i := n; i > 0; i = path[i] {
+		if isSpecial[i] {
+			c++
+			if last > 0 {
+				t := d[i] + d[n] - d[last] + 1
+				if t > res {
+					res = t
+				}
+			}
+
+			last = i
+		}
+	}
+
+	if c < k {
+		return d[n]
+	}
+
+	return res
 }
 
 func main() {
