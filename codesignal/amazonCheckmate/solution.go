bcce40a8a17bff7878880cd097e77b01c0c0b387
const <>
2020-03-20 23:29:22 -0700

    amazonCheckmate solved

--- codesignal/amazonCheckmate/solution.go
+++ codesignal/amazonCheckmate/solution.go
@@ -14,124 +14,94 @@ func amazonCheckmate(king, amazon string) []int {
 	kr, kc := int(king[0]-'a'), int(king[1]-'1')
 	ar, ac := int(amazon[0]-'a'), int(amazon[1]-'1')
 
-	isUnderAttack := func(r, c int) bool {
-		res := (r == kr && abs(c-kc) < 2) || (c == kc && abs(r-kr) < 2) || (abs(c-kc) == 1 && abs(r-kr) == 1) || (abs(r-ar)+abs(c-ac) == 3) //
-		if res {
-			return res
+	illegal := [8][8]bool{}
+	setIllegal := func(r, c int) {
+		if r >= 0 && c >= 0 && r < 8 && c < 8 {
+			illegal[r][c] = true
 		}
+	}
 
-		for x := ar - 1; x >= 0; x-- {
-			if x == kr && kc == ac {
+	isAttackedByKing := func(r, c int) bool {
+		return (abs(r-kr) <= 1 && abs(c-kc) <= 1)
+	}
+
+	// king
+	for r := kr - 1; r <= kr+1; r++ {
+		for c := kc - 1; c <= kc+1; c++ {
+			setIllegal(r, c)
+		}
+	}
+
+	// knight
+	for r := ar - 2; r <= ar+2; r++ {
+		for c := ac - 2; c <= ac+2; c++ {
+			if abs(r-ar)+abs(c-ac) == 3 {
+				setIllegal(r, c)
+			}
+		}
+	}
+
+	//queen
+	for k := -1; k <= 1; k += 2 {
+		//rows
+		for r := ar; r >= 0 && r < 8; r += k {
+			if r == kr && ac == kc {
 				break
 			}
-			if x == r && c == ac {
-				res = true
-			}
+			setIllegal(r, ac)
 		}
 
-		for x := ar + 1; x < 9; x++ {
-			if x == kr && kc == ac {
+		//columns
+		for c := ac; c >= 0 && c < 8; c += k {
+			if c == kc && ar == kr {
 				break
 			}
-			if x == r && c == ac {
-				res = true
+			setIllegal(ar, c)
+		}
+	}
+
+	for i := -1; i <= 1; i += 2 {
+		for j := -1; j <= 1; j += 2 {
+			for r, c := ar, ac; r >= 0 && c >= 0 && r < 8 && c < 8; r, c = i+r, j+c {
+				if c == kc && r == kr {
+					break
+				}
+				setIllegal(r, c)
 			}
 		}
-
-		for y := ac - 1; y >= 0; y-- {
-			if y == kc && kr == r {
-				break
-			}
-			if c == ac && y == ar {
-				res = true
-			}
-		}
-
-		for y := ac + 1; y < 8; y++ {
-			if y == kc && kr == r {
-				break
-			}
-			if c == ac && y == ar {
-				res = true
-			}
-		}
-
-		x, y := ar-1, ac-1
-		for x >= 0 && y >= 0 && x < 8 && y < 8 {
-			if x == kr && y == kc {
-				break
-			}
-			res = res || (x == r && y == c)
-
-			x--
-			y--
-		}
-
-		x, y = ar-1, ac+1
-		for x >= 0 && y >= 0 && x < 8 && y < 8 {
-			if x == kr && y == kc {
-				break
-			}
-			res = res || (x == r && y == c)
-
-			x--
-			y++
-		}
-
-		x, y = ar+1, ac-1
-		for x >= 0 && y >= 0 && x < 8 && y < 8 {
-			if x == kr && y == kc {
-				break
-			}
-			res = res || (x == r && y == c)
-
-			x++
-			y--
-		}
-
-		x, y = ar+1, ac+1
-		for x >= 0 && y >= 0 && x < 8 && y < 8 {
-			if x == kr && y == kc {
-				break
-			}
-			res = res || (x == r && y == c)
-
-			x++
-			y++
-		}
-
-		return res
 	}
 
 	// checkmate positions, check positions, stalemate positions, safe positions
 	var mate, check, stale, safe int
 	for r := 0; r < 8; r++ {
 		for c := 0; c < 8; c++ {
-			if r == kr && c == kc || r == ar && c == ac {
+			if isAttackedByKing(r, c) || r == ar && c == ac {
 				continue
 			}
 
 			canMove := false
 			for dr := -1; dr < 2; dr++ {
 				for dc := -1; dc < 2; dc++ {
-					if dr == 0 && dc == 0 || r+dr < 0 || r+dr > 7 || c+dc < 0 || c+dc > 7 {
+					r1, c1 := r+dr, c+dc
+					if dr == 0 && dc == 0 || r1 < 0 || r1 > 7 || c1 < 0 || c1 > 7 {
 						continue
 					}
 
-					canMove = canMove || !isUnderAttack(r+dr, c+dc)
+					canMove = canMove || !(illegal[r1][c1] && (isAttackedByKing(r1, c1) || r1 != ar || c1 != ac))
 				}
 			}
 
-			if isUnderAttack(r, c) {
+			if illegal[r][c] {
 				if canMove {
 					check++
 				} else {
 					mate++
+					//fmt.Print(string('a'+r)+string('1'+c), " ")
 				}
 			} else {
 				if canMove {
 					safe++
-					fmt.Print(string('a'+r)+string('1'+c), " ")
+					//fmt.Print(string('a'+r)+string('1'+c), " ")
 				} else {
 					stale++
 				}
@@ -146,4 +116,5 @@ func amazonCheckmate(king, amazon string) []int {
 
 func main() {
 	fmt.Println(amazonCheckmate("d3", "e4"))
+	fmt.Println(amazonCheckmate("a1", "g5"))
 }
