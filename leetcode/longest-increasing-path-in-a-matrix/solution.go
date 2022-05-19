9b09464399d285466e62803f2e3cf125784a319a
const <Khvalin@gmail.com>
2022-05-19 00:45:35 -0700

    some tests

--- leetcode/longest-increasing-path-in-a-matrix/solution.go
+++ leetcode/longest-increasing-path-in-a-matrix/solution.go
@@ -33,11 +33,11 @@ func longestIncreasingPath(matrix [][]int) int {
 				continue
 			}
 
-			key := dx*nmax + dy
+			key2 := dx*nmax + dy
 
 			d := 0
 			ps := &s.dist
-			if v, f := s.dist[key]; f {
+			if v, f := s.dist[key2]; f {
 				d = v + 1
 			} else {
 				next := state{dx, dy, map[int]int{}}
@@ -51,7 +51,7 @@ func longestIncreasingPath(matrix [][]int) int {
 
 			if d > res {
 				res = d
-				s.dist[key] = d
+				//s.dist[key2] = d
 				st = ps
 			}
 		}
@@ -75,11 +75,10 @@ func longestIncreasingPath(matrix [][]int) int {
 }
 
 func main() {
-	input := [][]int{
-		{9, 9, 4},
-		{6, 6, 8},
-		{2, 1, 1},
-	}
+	input := [][]int{{9, 8, 7}, {4, 5, 6}, {3, 2, 1}}
+	fmt.Println(longestIncreasingPath(input), 9)
+
+	input = [][]int{{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}, {19, 18, 17, 16, 15, 14, 13, 12, 11, 10}, {20, 21, 22, 23, 24, 25, 26, 27, 28, 29}, {39, 38, 37, 36, 35, 34, 33, 32, 31, 30}, {40, 41, 42, 43, 44, 45, 46, 47, 48, 49}, {59, 58, 57, 56, 55, 54, 53, 52, 51, 50}, {60, 61, 62, 63, 64, 65, 66, 67, 68, 69}, {79, 78, 77, 76, 75, 74, 73, 72, 71, 70}, {80, 81, 82, 83, 84, 85, 86, 87, 88, 89}, {99, 98, 97, 96, 95, 94, 93, 92, 91, 90}, {100, 101, 102, 103, 104, 105, 106, 107, 108, 109}, {119, 118, 117, 116, 115, 114, 113, 112, 111, 110}, {120, 121, 122, 123, 124, 125, 126, 127, 128, 129}, {139, 138, 137, 136, 135, 134, 133, 132, 131, 130}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0}}
 
 	fmt.Println(longestIncreasingPath(input))
 }
