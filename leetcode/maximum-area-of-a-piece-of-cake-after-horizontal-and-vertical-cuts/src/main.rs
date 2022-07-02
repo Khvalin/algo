caae241cd1a8227ba0da6b2cf293cc29a0c890ae
const <Khvalin@gmail.com>
2022-07-02 02:23:15 -0700

    rust: 1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ leetcode/maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/src/main.rs
@@ -0,0 +1,36 @@
+struct Solution;
+
+impl Solution {
+    pub fn max_area(h: i32, w: i32, horizontal_cuts: Vec<i32>, vertical_cuts: Vec<i32>) -> i32 {
+        fn sort<A, T>(mut array: A) -> A
+        where
+            A: AsMut<[T]>,
+            T: Ord,
+        {
+            let slice = array.as_mut();
+            slice.sort();
+
+            array
+        }
+
+        let mut hc: Vec<i32> = sort(horizontal_cuts);
+        let mut vc: Vec<i32> = sort(vertical_cuts);
+        hc.sort();
+        vc.sort();
+        let mut hmax = Ord::max(hc[0], h - hc[hc.len() - 1]);
+        for i in 1..=hc.len() - 1 {
+            hmax = Ord::max(hmax, hc[i] - hc[i - 1])
+        }
+
+        let mut vmax = Ord::max(vc[0], w - vc[vc.len() - 1]);
+        for i in 1..=vc.len() - 1 {
+            vmax = Ord::max(vmax, vc[i] - vc[i - 1])
+        }
+
+        return (((vmax as u64) * (hmax as u64)) % 1000000007) as i32;
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
