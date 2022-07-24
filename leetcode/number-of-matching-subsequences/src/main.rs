fb7d27c185c3778b8b297a99e09c1cebc406a105
const <Khvalin@gmail.com>
2022-07-24 00:48:28 -0700

    number-of-matching-subsequences rust arrat

---
+++ leetcode/number-of-matching-subsequences/src/main.rs
@@ -0,0 +1,54 @@
+struct Solution;
+
+impl Solution {
+    pub fn num_matching_subseq(s: String, words: Vec<String>) -> i32 {
+        let mut next: Vec<Vec<usize>> = vec![];
+        let mut counter: Vec<usize> = words.to_owned().into_iter().map(|_| 0).collect();
+
+        for _ in 0..('z' as usize) + 1 {
+            let a: Vec<usize> = vec![];
+            next.push(a);
+        }
+
+        for i in 0..words.len() {
+            let ch = words[i].chars().nth(0).unwrap();
+
+            next[ch as usize].push(i);
+        }
+        let mut res = 0;
+
+        for ch in s.chars() {
+            let a = next[ch as usize].to_owned();
+            next[ch as usize].clear();
+
+            for i in a {
+                counter[i] += 1;
+                if counter[i] >= words[i].len() {
+                    res += 1;
+                    continue;
+                }
+
+                let c = words[i].chars().nth(counter[i]).unwrap();
+
+                next[c as usize].push(i);
+            }
+        }
+
+        res
+    }
+}
+
+fn main() {
+    let res = Solution::num_matching_subseq(
+        "abcdeb".to_string(),
+        vec![
+            "a".to_string(),
+            "bb".to_string(),
+            "acd".to_string(),
+            "ace".to_string(),
+            "ahce".to_string(),
+        ],
+    );
+
+    println!("{}", res);
+}
