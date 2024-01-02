b80e189359cb412849daf36c0f625b7027aa6689
const <Khvalin@gmail.com>
2024-01-01 23:59:37 -0800

    convert-an-array-into-a-2d-array-with-conditions take 2

--- leetcode/rust-solutions/src/problems/convert_an_array_into_a_2d_array_with_conditions.rs
+++ leetcode/rust-solutions/src/problems/convert_an_array_into_a_2d_array_with_conditions.rs
@@ -2,13 +2,13 @@ struct Solution;
 
 impl Solution {
     pub fn find_matrix(nums: Vec<i32>) -> Vec<Vec<i32>> {
-        let mut res = vec![];
+        let mut res = Vec::with_capacity(nums.len());
         let mut count = [0; 201];
 
         for n in &nums {
             let c = count[*n as usize];
             if c >= res.len() {
-                res.push(vec![]);
+                res.push(Vec::with_capacity(nums.len()));
             }
             res[c].push(*n);
 
